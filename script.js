ae8f6274e3a654dc0a892371a78715b6fbf60258
timeisnowandnotlater <130266233+timeisnowandnotlater@users.noreply.github.com>
2023-09-14 18:34:28 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+function game() {
+document.getElementById("game").style.display = "block";
+document.getElementById("qu").style.display = "none";
+document.getElementById("bun").style.display = "none";
+document.getElementById("hello").style.display = "none";
+}
+function fix() {
+document.getElementById("game").style.display = "none";
+}
